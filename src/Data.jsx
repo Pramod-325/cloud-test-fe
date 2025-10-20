618a16d81ff7446c1bd447ab115b96794ee53b11
pramod-325 <vpramodkumar2003@gmail.com>
2025-10-20 09:08:45 +0530

    added data component

---
+++ src/Data.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+
+function Data() {
+  const [data, setData] = useState([]);
+  const [ips, setIps] = useState(["test IP-1", "test IP-2"]);
+  useEffect(()=>{
+    const k = async()=>{
+        const res = await fetch("/api/data");
+        const da = await res.json();
+        console.log(da.data);
+        setIps(ips.concat(da.ips))
+        setData(da.data);
+    };
+    k();
+  },[])
+
+  if(!data || data.length===0){
+    return (
+      <div className="p-6 bg-white shadow-xl rounded-xl w-full max-w-lg text-center">
+        <h2 className="text-xl font-medium text-blue-600 animate-pulse">
+          Loading Data...
+        </h2>
+      </div>
+    )
+  }
+  return (
+    <>
+        <h1>Found Data</h1>
+        {data.map((x, index) => (
+          <li className="text-gray-600 bg-gray-50 p-2 rounded-lg transition duration-150 hover:bg-blue-50">
+            {x}
+          </li>
+        ))}
+        {ips.map((x, index) => (
+          <li className="text-gray-600 bg-gray-50 p-2 rounded-lg transition duration-150 hover:bg-blue-50">
+            {x}
+          </li>
+        ))}
+
+    </>
+  )
+}
+
+export default Data;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import './index.css'
 import App from './App.jsx'
+import Data from './Data.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+         <Route path="/" element={<App />} />
+         <Route path="/data" element={<Data />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
